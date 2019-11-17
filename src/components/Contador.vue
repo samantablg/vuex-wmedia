92232f92eb7761620a770ec28e63207d4c9dd0c2
Samanta Belen Lara <samanta.lara@plexus.es>
2019-11-17 18:26:53 +0000

    first commit

---
+++ src/components/Contador.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <div>
+        <h1>Contador: {{ cantidad }}</h1>
+        <button @click="aumentar">Aumentar</button>
+        <button @click="reducir">Reducir</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  // props: ['cantidad'],
+  methods: {
+    aumentar() {
+      /* this.$emit('aumentar'); */
+      this.$store.state.cantidad++;
+    },
+    reducir() {
+      /* this.$emit('reducir'); */
+      this.$store.state.cantidad--;
+    }
+  },
+  computed: {
+    cantidad() {
+      return this.$store.sate.cantidad;
+    }
+  }
+
+}
+</script>
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+// esta será el almacén centralizado
+import Vue from 'vue';
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        cantidad: 0
+    }
+})
